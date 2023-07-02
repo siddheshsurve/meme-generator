36516a313cb3774ad552dad51bf72513727a2e2d
Siddhesh Surve <sysurve24@gmail.com>
2023-07-02 14:22:54 +0000

    memesData document

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from "react";
+import memesData from "../memesData";
 
 const Meme = () => {
+    let imgUrl; //globally declared so as to use in return
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    //get a random number which is in between range of length of array
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+
+    //get url property of random memesData record in imgUrl
+    imgUrl = memesArray[randomNumber].url;
+  }
+
   return (
     <main>
-        <form className='form'>
-            <input type="text" className='form--input' placeholder='Top text' />
-            <input type="text" className='form--input' placeholder='Bottom text' />
-            <button className='form--button'>Get a new meme image 🖼</button>
-        </form>
+      <div className="form">
+        <input type="text" className="form--input" placeholder="Top text" />
+        <input type="text" className="form--input" placeholder="Bottom text" />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
     </main>
-  )
-}
+  );
+};
 
 export default Meme;
