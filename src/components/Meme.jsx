b7bf05383d5527fefe8b2b58a563e07def6025a7
Siddhesh Surve <sysurve24@gmail.com>
2023-07-03 13:43:52 +0000

    usestate image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import memesData from "../memesData";
 
 const Meme = () => {
-    let imgUrl; //globally declared so as to use in return
+    
+  // let imgUrl; //globally declared so as to use in return
+  const [memeImage, setMemeImage] = React.useState("")
   function getMemeImage() {
     const memesArray = memesData.data.memes;
     //get a random number which is in between range of length of array
     const randomNumber = Math.floor(Math.random() * memesArray.length)
-
+    setMemeImage(memesArray[randomNumber].url);
     //get url property of random memesData record in imgUrl
-    imgUrl = memesArray[randomNumber].url;
+    // imgUrl = memesArray[randomNumber].url;
   }
 
   return (
@@ -21,6 +23,7 @@ const Meme = () => {
           Get a new meme image 🖼
         </button>
       </div>
+      <img className="meme--image" src={memeImage} />
     </main>
   );
 };
